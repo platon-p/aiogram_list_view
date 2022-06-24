489a2fee91b1580429d67c84cfb68e6feb152682
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-06-24 11:54:52 +0300

    add custom buttons and change arrows

--- list_view.py
+++ list_view.py
@@ -18,12 +18,14 @@ class ListView:
         self, 
         items: Iterable[Union[str, int]], 
         row_width: int = 1, 
-        is_enumerate: bool = False
+        is_enumerate: bool = False,
+        buttons: Iterable[InlineKeyboardButton] = []
     ) -> None:
         """
         items - items for display
         row_width - how many items should be displayed in a row
         is_enumerate - should items be enumerated
+        buttons - list of buttons which should be displayed below, after ListView's buttons
         """
 
         self.items = [
@@ -32,10 +34,17 @@ class ListView:
         ]
         self.row_width = row_width
         self.is_enumerate = is_enumerate
+        self.buttons = buttons
 
         ListView.lv_unique_id += 1
         self._lv_unique_id = ListView.lv_unique_id
         self._callback_data_name = f"_ListViewItems{self._lv_unique_id}"
+    
+    def get_lv_unique_id(self) -> int:
+        return self._lv_unique_id
+    
+    def get_callback_data_name(self) -> str:
+        return self._callback_data_name
 
     def get_page(self, page_num: int = 0) -> InlineKeyboardMarkup:
         keyboard = InlineKeyboardMarkup()
@@ -53,7 +62,7 @@ class ListView:
         if len(self.items) > 8:
             keyboard.add(
                 InlineKeyboardButton(
-                    '<-' if page_num > 0 else ' ',
+                    '⟵' if page_num > 0 else ' ',
                     callback_data=(
                         f'{self._callback_data_name}_toPage_{page_num - 1}' 
                         if page_num > 0 else 
@@ -65,7 +74,7 @@ class ListView:
                     callback_data=f'{self._callback_data_name}_toPage_{0 if page_num != 0 else len(self.items) // 8}'
                 ),
                 InlineKeyboardButton(
-                    '->' if (page_num * 8 + 8) < len(self.items) else ' ',
+                    '⟶' if (page_num * 8 + 8) < len(self.items) else ' ',
                     callback_data=(
                         f'{self._callback_data_name}_toPage_{page_num + 1}' 
                         if (page_num * 8 + 8) < len(self.items) else 
@@ -73,6 +82,9 @@ class ListView:
                     )
                 )
             )
+
+        for button in self.buttons:
+            keyboard.add(button)
 
         return keyboard
 
@@ -91,9 +103,6 @@ class ListView:
         page_num = item_num // 8
         return self.get_page(page_num)
 
-    def get_item_page(self, item_id: int) -> InlineKeyboardMarkup:
-        print("типа нажатие на кнопку")
-
 
     async def to_page(self, callback_query: types.CallbackQuery) -> None:
         page_num = int(callback_query.data.split('_')[-1])
@@ -102,8 +111,7 @@ class ListView:
 
     async def get_item_page_handler(self, callback_query: types.CallbackQuery) -> None:
         item_id = int(callback_query.data.split('_')[-1])
-        keyboard = self.get_item_page(item_id)
-        await callback_query.message.edit_text("text", reply_markup=keyboard)
+        await callback_query.message.edit_text(self.items[item_id][1], reply_markup=None)
 
     def get_handlers(self) -> List[Tuple[Callable, Callable]]:
         handlers = [
